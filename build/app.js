bd4a4ec68a5c2455579b01ebdaa918005f352465
kirbyclopez <kirbyclopez@gmail.com>
2023-01-05 22:17:53 +0800

    Updated build

--- build/app.js
+++ build/app.js
@@ -12,7 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production")
+    require("dotenv").config();
 const connect_1 = __importDefault(require("./utils/connect"));
 const logger_1 = __importDefault(require("./utils/logger"));
 const server_1 = __importDefault(require("./utils/server"));
